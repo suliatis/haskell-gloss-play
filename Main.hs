2066b008903a96ce3fdb2666aa7512ef8ac4061d
Attila Süli <suli.zakar.attila@gmail.com>
2020-08-16 15:25:21 +0200

    The player can't step further the playground

--- Main.hs
+++ Main.hs
@@ -12,18 +12,21 @@ data Player = Player
   { position  :: (Float, Float)
   , size      :: Float
   , direction :: Direction
-  , step      :: Step
   }
 
-isLeaving :: Player -> Playground -> Bool
-isLeaving p pg =
+maxStep :: Player -> Playground -> Step -> Step
+maxStep p pg (Step step) =
   let
     (x, y) = position p
+    horizontalStep = min step $ fromIntegral (width pg) / 2 - (size p) / 2 - abs x
+    verticalStep = min step $ fromIntegral (height pg) / 2 - (size p) / 2 - abs y
   in
-    x > fromIntegral (width pg) / 2 - (size p) / 2
-    || x < -(fromIntegral (width pg) / 2 - (size p) / 2)
-    || y > fromIntegral (height pg) / 2 - (size p) / 2
-    || y < -(fromIntegral (height pg) / 2 - (size p) / 2)
+  Step $ case (direction p) of
+           Direction'Left  | x < 0 -> horizontalStep
+           Direction'Right | x > 0 -> horizontalStep
+           Direction'Up    | y > 0 -> verticalStep
+           Direction'Down  | y < 0 -> verticalStep
+           _                       -> step
 
 data Direction
   = Direction'Left
@@ -32,16 +35,15 @@ data Direction
   | Direction'Right
   | Direction'No
 
-move :: Player -> Player
-move p =
+move :: Player -> Step -> Player
+move p (Step step) =
   let
     (x, y) = position p
-    Step stepValue = step p
     (x', y') = case (direction p) of
-                 Direction'Left  -> (x - stepValue, y)
-                 Direction'Down  -> (x, y - stepValue)
-                 Direction'Up    -> (x, y + stepValue)
-                 Direction'Right -> (x + stepValue, y)
+                 Direction'Left  -> (x - step, y)
+                 Direction'Down  -> (x, y - step)
+                 Direction'Up    -> (x, y + step)
+                 Direction'Right -> (x + step, y)
                  Direction'No    -> (x, y)
   in
     p { position = (x', y') }
@@ -64,7 +66,6 @@ main = do
         { position = (0, 0)
         , size = 20
         , direction = Direction'No
-        , step = Step 5
         }
       }
     play FullScreen white 60 world draw update tick
@@ -73,7 +74,7 @@ draw :: World -> Picture
 draw world = pictures $ [ drawPlayer $ player world ]
 
 drawPlayer :: Player -> Picture
-drawPlayer (Player (x, y) s _ _) =
+drawPlayer (Player (x, y) s _) =
   translate x y $ color black $ rectangleSolid s s
 
 update :: Event -> World -> World
@@ -93,6 +94,7 @@ update _ world = world
 tick :: Float -> World -> World
 tick _ (World pg p) =
   let
-    p' = if isLeaving (move p) pg then p else move p
+    step = maxStep p pg $ Step 10
+    p' = move p step
   in
     World pg p'
